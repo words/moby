bc237b956b0ae0b74c639d99a79dcb23fe849282
Zeke Sikelianos <zeke@sikelianos.com>
2020-12-18 17:29:20 -0800

    redirect http to https

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const app = module.exports = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(path.join(__dirname, '/public')))
-
 app.use('/', express.static('public'))
 app.set('view engine', 'pug')
+
+// redirect HTTP requests to HTTPS
+app.get('*', (req, res, next) => {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect('https://moby-thesaurus.org' + req.url)
+  } else {
+    next()
+  }
+})
 
 app.get('/', function (req, res) {
   res.render('index')
