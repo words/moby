aee0cbb1c1c20fe4844c33a63ceb7c6ee7200fd0
Zeke Sikelianos <zeke@sikelianos.com>
2020-12-18 17:50:11 -0800

    add tracking code for gaug.es

--- public/app.js
+++ public/app.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   document.querySelector('#q').focus()
+
+  var _gauges = _gauges || []; // eslint-disable-line
+  (function () {
+    const t = document.createElement('script')
+    t.type = 'text/javascript'
+    t.async = true
+    t.id = 'gauges-tracker'
+    t.setAttribute('data-site-id', '5fdd5b6c1d04c241c2856e6f')
+    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif')
+    t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js'
+    const s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(t, s)
+  })()
 })
